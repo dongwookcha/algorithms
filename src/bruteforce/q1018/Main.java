c93419d81a7db141340a6c02cca24d70d7e3f41f
dongwook <dongwookcha0@gmail.com>
2020-08-05 02:15:27 +0900

    Skip at this point

--- src/bruteforce/q1018/Main.java
+++ src/bruteforce/q1018/Main.java
@@ -1,4 +1,27 @@
 package bruteforce.q1018;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = in.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        char[][] board = new char[N][M];
+        for(int i = 0; i < N; i++) {
+            String line = in.readLine();
+            for(int j = 0; j < M; j++) {
+                board[i][j] = line.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            System.out.println(Arrays.toString(board[i]));
+        }
+    }
 }
