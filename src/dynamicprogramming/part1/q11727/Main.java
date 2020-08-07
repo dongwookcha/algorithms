a74a3cdd4436617ba66a6feb69d1295742535e07
dongwook <dongwookcha0@gmail.com>
2020-08-07 17:57:14 +0900

    Complete DP Questions

--- src/dynamicprogramming/part1/q11727/Main.java
+++ src/dynamicprogramming/part1/q11727/Main.java
@@ -1,4 +1,24 @@
 package dynamicprogramming.part1.q11727;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int[] d;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        d = new int[n + 1];
+        System.out.println(dp(n));
+    }
+
+    private static int dp(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 3;
+        if (d[n] > 0) return d[n];
+
+        d[n] = dp(n - 1) + 2 * dp(n - 2);
+        d[n] %= 10007;
+
+        return d[n];
+    }
 }
--- src/dynamicprogramming/part1/q9095/Main.java
+++ src/dynamicprogramming/part1/q9095/Main.java
@@ -1,4 +1,29 @@
 package dynamicprogramming.part1.q9095;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int[] d;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while(T-- > 0) {
+            int n = sc.nextInt();
+            d = new int[n + 1];
+            System.out.println(dp(n));
+        }
+
+    }
+
+    private static int dp(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (d[n] > 0) return d[n];
+
+        d[n] = dp(n - 1) + dp(n - 2) + dp(n - 3);
+
+        return d[n];
+    }
 }
