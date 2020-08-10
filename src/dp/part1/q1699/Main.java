6fb290e741ed0ef292fa5a1b32d85d011e319ce7
dongwook <dongwookcha0@gmail.com>
2020-08-10 21:33:28 +0900

    Complete DP Questions

--- src/dp/part1/q1699/Main.java
+++ src/dp/part1/q1699/Main.java
@@ -1,4 +1,23 @@
 package dp.part1.q1699;
 
+import java.util.Scanner;
+
 public class Main {
+    static int[] d;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        d = new int[n + 1];
+
+        for (int i=1; i<=n; i++) {
+            d[i] = i;
+            for (int j=1; j*j <= i; j++) {
+                if (d[i] > d[i-j*j]+1) {
+                d[i] = d[i-j*j]+1;
+                }
+            }
+        }
+
+        System.out.println(d[d.length - 1]);
+    }
 }
--- src/dp/part1/q1912/Main.java
+++ src/dp/part1/q1912/Main.java
@@ -1,8 +1,37 @@
 package dp.part1.q1912;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static int a[]; // storage
+    static int d[];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        a = new int[n];
+        d = new int[n];
+        int max;
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            d[i] = a[i];
+            if (i == 0) continue;
+            if (d[i] < d[i - 1] + a[i]) {
+                d[i] = d[i - 1] + a[i];
+            }
+        }
+
+        max = d[0];
+        for (int i = 1; i < d.length; i++) {
+            if (max < d[i]) {
+                max = d[i];
+            }
+        }
+
+        System.out.println(max);
     }
 
 }
