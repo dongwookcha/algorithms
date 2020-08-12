680d78711b4a2881a2419554ed3b86a67d51656f
dongwook <dongwookcha0@gmail.com>
2020-08-13 03:08:19 +0900

    Complete DP Questions

--- src/dp/part1/q1932/Main.java
+++ src/dp/part1/q1932/Main.java
@@ -1,7 +1,28 @@
 package dp.part1.q1932;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static int[][] d;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        d = new int[n+1][n+1];
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                d[i][j] = sc.nextInt();
+
+                if(j == 1) d[i][j] += d[i-1][j];
+                else if(j == i) d[i][j] += d[i-1][j-1];
+                else d[i][j] = Math.max((d[i][j] + d[i-1][j]), (d[i][j] + d[i-1][j-1]));
+
+                if (sum < d[i][j]) sum = d[i][j];
+            }
+        }
+
+        System.out.println(sum);
     }
 }
--- src/dp/part1/q2579/Main.java
+++ src/dp/part1/q2579/Main.java
@@ -1,4 +1,35 @@
 package dp.part1.q2579;
 
+import java.util.Scanner;
+
 public class Main {
+    static int[] d;
+    static int[] a;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        a = new int[n+1];
+        d = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println(dp(n));
+    }
+
+    private static int dp(int n) {
+        d[1] = a[1];
+
+        if (n >= 2) {
+          d[2] = a[1] + a[2];
+        }
+
+        for (int i = 3; i <= n; i++) {
+            d[i] = Math.max(d[i-2] + a[i], d[i-3] + a[i] + a[i-1]);
+        }
+
+        return d[n];
+    }
 }
