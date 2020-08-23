2034dddf833d87437468cddea3d39535ed9e3bc7
dongwook <dongwookcha0@gmail.com>
2020-08-24 01:03:35 +0900

    Practice DFS and BFS in case of Graph

--- src/graph/BFS.java
+++ src/graph/BFS.java
@@ -1,7 +1,30 @@
 package graph;
 
-public class BFS {
-    public static void main(String[] args) {
+import java.util.Queue;
+import java.util.Scanner;
 
+public class BFS {
+    static int[][] a;
+    static Queue<Integer> q;
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        a = new int[n+1][n+1];
+        check = new boolean[n+1];
+
+        check[1] = true;
+        q.add(1);
+
+        while(!q.isEmpty()) {
+            int x = q.poll();
+            for(int i=1; i<=n; i++) {
+                if(a[x][i] == 1 && check[i] == false) {
+                    check[i] = true;
+                    q.add(i);
+                }
+            }
+        }
     }
 }
--- src/graph/DFS.java
+++ src/graph/DFS.java
@@ -1,7 +1,44 @@
 package graph;
 
-public class DFS {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class DFS {
+    static int[][] a; // for adjacent matrix
+    static ArrayList<Integer>[] g; // for adjacent list
+    static boolean[] check;
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        a = new int[n+1][n+1];
+        g = (ArrayList<Integer>[])  new ArrayList[n];
+        check = new boolean[n+1];
+
+        for(int i = 1; i < n; i++) {
+
+        }
+
+    }
+
+    // use Adjacent Matrix
+    public static void DFS_M(int x) {
+        check[x] = true;
+        for(int i = 0; i <= n; i++) {
+            if(a[x][i] == 1 && check[i] == false) {
+                DFS_M(i);
+            }
+        }
+    }
+
+    // use Adjacent List
+    public static void DFS_L(int x) {
+        check[x] = true;
+        for(int i = 0; i < g[x].size(); i++) {
+            int y = a[x][i];
+            if(check[y] == false) {
+                DFS_L(y);
+            }
+        }
     }
 }
