c4e894394c29e2a56a9378d497895cf0cd1fe929
dongwook <dongwookcha0@gmail.com>
2020-08-25 01:07:34 +0900

    Complete Graph Questions

--- src/graph/q1260/Main.java
+++ src/graph/q1260/Main.java
@@ -1,56 +1,54 @@
 package graph.q1260;
+
 import java.util.*;
 
 public class Main {
     static int[][] a;
     static boolean[] c;
+    static int n;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         int m = sc.nextInt();
         int v = sc.nextInt();
-//        ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[n+1];
+
         a = new int[n+1][n+1];
         c = new boolean[n+1];
 
-//        for(int i = 1; i <= n; i++) {
-//            a[i] = new ArrayList<>();
-//        }
-
-        for(int i = 1; i <= m; i++) {
+        for(int i = 0; i < m; i++) {
             int from = sc.nextInt();
             int to = sc.nextInt();
             a[from][to] = 1;
             a[to][from] = 1;
         }
 
-        dfs(v, n);
+        dfs(v);
         System.out.println();
         Arrays.fill(c, false);
-        bfs(v, n);
+        bfs(v);
     }
 
-    public static void dfs(int v, int n) {
-        c[v] = true;
-        System.out.print(v + " ");
+    public static void dfs(int x) {
+        c[x] = true;
+        System.out.print(x + " ");
+
         for(int i = 1; i <= n; i++) {
-            if(c[i] == false && a[v][i] == 1) {
-                dfs(i, n);
+            if(c[i] == false && a[x][i] == 1) {
+                dfs(i);
             }
         }
 
     }
 
-    public static void bfs(int v, int n) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.offer(v);
-        c[v] = true;
+    public static void bfs(int x) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(x);
+        c[x] = true;
 
         while(!q.isEmpty()) {
             int first = q.poll();
             System.out.print(first + " ");
-
             for(int i = 1; i <= n; i++) {
                 if(a[first][i] == 1 && c[i] == false) {
                     q.offer(i);
